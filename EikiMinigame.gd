165e3c7f2faa9d316a21ddfd6a27f54e1715544b
CauaRebelo <cauarebelo@discente.ufg.br>
2020-01-18 21:17:36 -0400

    Instanciação do inimigo completa

--- EikiMinigame.gd
+++ EikiMinigame.gd
@@ -11,21 +11,18 @@ func _ready():
 	$AnimatedSprite.animation = "default"
 
 func _input(event):
-	if Input.is_action_pressed("ui_right"):
-		$AnimatedSprite.animation = "right"
-		emit_signal("heaven")
-	if Input.is_action_pressed("ui_left"):
-		$AnimatedSprite.animation = "left"
-		emit_signal("hell")
-
-func _process(delta):
-	if(win == 1):
-		$AnimatedSprite.animation = "victory"
-	if(loss == 1):
-		$AnimatedSprite.animation = "loss"
+	if(win == 0 || loss == 0):
+		if Input.is_action_pressed("ui_right"):
+			$AnimatedSprite.animation = "right"
+			emit_signal("heaven")
+		if Input.is_action_pressed("ui_left"):
+			$AnimatedSprite.animation = "left"
+			emit_signal("hell")
 
 func _on_Minigame2_win():
 	win = 1
+	$AnimatedSprite.animation = "victory"
 
 func _on_Minigame2_fail():
 	loss = 1
+	$AnimatedSprite.animation = "default"
--- Minigame2.gd
+++ Minigame2.gd
@@ -1,17 +1,23 @@
-extends Node
+extends Node2D
 
 signal fail
 signal win
+signal hell
+signal heaven
 
 var enemies = 3 #(totalloop - 1) + 3
 
-const judged = preload("res://Cena1_Enemy.tscn")
+const judged = preload("res://Cena2_Enemy.tscn")
 
 func _ready():
 	var enemy = judged.instance()
-	get_node("Minigame2").add_child(enemy)
-	$Minigame2.connect("fail", enemy, "stopTimer")
-	$Minigame2.connect("win", enemy, "stopTimer")
+	add_child(enemy)
+	connect("fail", enemy, "stopTimer")
+	connect("win", enemy, "stopTimer")
+	$Eiki.connect("hell", enemy, "_on_Eiki_hell")
+	$Eiki.connect("heaven", enemy, "_on_Eiki_heaven")
+	enemy.connect("correct", self, "_on_Node2D_correct")
+	enemy.connect("wrong", self, "_on_Node2D_wrong")
 	$TotalTimer.start()
 	$LossLabel.hide()
 	$WinLabel.hide()
@@ -19,6 +25,7 @@ func _ready():
 func _on_TotalTimer_timeout():
 	emit_signal("fail")
 	$LossLabel.show()
+
 func _on_Node2D_wrong():
 	emit_signal("fail")
 	$LossLabel.show()
